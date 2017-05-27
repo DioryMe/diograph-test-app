ea935aae9e61bb1bd43b69b7b35b98c1b0564ee3
Jouni Alanen <jvalanen@gmail.com>
2017-05-27 16:45:22 +0300

    Initial test app

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!")
+import { DiographStore } from "diograph-store"
+import { DiographAuthentication } from "diograph-authentication"
+
+// DiographStore.setAuthToken("test-token")
+DiographStore.setAuthToken(DiographAuthentication.token);
+
+try {
+DiographStore.get("5691").then((diory) => {
+  console.log(diory)
+})
+} catch(e) {
+  console.log(e)
+}

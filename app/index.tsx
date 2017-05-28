aa4354ddc708aac4dca2cf768bc354601dcbd657
Jouni Alanen <jvalanen@gmail.com>
2017-05-28 07:26:35 +0300

    Renders a Diory component

--- app/index.tsx
+++ app/index.tsx
@@ -8,11 +8,23 @@ import { DiographAuthentication } from "diograph-authentication"
 DiographStore.setAuthToken(DiographAuthentication.token);
 
 try {
-  DiographStore.get("5691").then((diory) => {
+  DiographStore.get("5691").then((dioryData) => {
+
+    const diory = {
+      text: dioryData.name,
+      image: 'https://gravatar.com/avatar/ff80f8f9bc52f1b79e468a41f2239001',
+      styles: {
+        diory: { display: 'inline-block', width: '20em', height: '20em', backgroundColor: 'green', margin: '1em' },
+        text: { fontSize: '2em', fontFamily: 'sans-serif', color: 'white', textAlign: 'center', textShadow: '1px 1px green' },
+        image: { opacity: 0.6, filter: 'blur(5px)' }
+      }
+    }
+
     ReactDOM.render(
-      <h1>{ diory.name }</h1>,
+      <Diory { ...diory } />,
       document.getElementById('app')
     );
+
   })
 } catch(e) {
   console.log(e)

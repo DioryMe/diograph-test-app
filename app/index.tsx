6ea1a3c6d54f9b3a2b9bec9c04c5fa279bd9fd97
Jouni Alanen <jouni.alanen@leadin.fi>
2017-08-30 22:40:11 +0300

    diograph-search-create component copy-pasted over as a React SearchCreate

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from 'react-dom';
 import { Diory, DioryGrid } from 'diory-react-components'
 import { DiographStore } from "diograph-store"
 import { DiographAuthentication } from "diograph-authentication"
+import * as $ from "jquery";
+import * as Bloodhound from "corejs-typeahead/dist/bloodhound";
+import "corejs-typeahead/dist/typeahead.jquery";
 
 render()
 
@@ -42,6 +45,8 @@ function render() {
         document.getElementById('app')
       );
 
+      initializeTypeahead()
+
     })
   } catch(e) {
     console.log(e)
@@ -56,5 +61,73 @@ function clear() {
 }
 
 const SearchCreate = () => {
-  return <input />
+  return <div id="search-create">
+          <input id="typeahead" class="typeahead" type="text" placeholder="Search for diories..." />
+          <div id='loading-icon'>
+            <img src='loading.gif' />
+          </div>
+          <div id="search-create__cancel" class='search-create__cancel'>X</div>
+        </div>
 }
+
+function initializeTypeahead() {
+    var searchResults = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      remote: {
+        url: 'http://diory-server.herokuapp.com/v1/search',
+        prepare: function(query, settings) {
+          settings.url = settings.url + '?q=' + query,
+          settings.headers = {
+            "Authorization": "test-token"
+          };
+          return settings;
+        }
+      }
+    });
+
+    $('#search-create #typeahead').typeahead({
+        hint: false,
+        minLength: 3
+      }, {
+        display: 'value',
+        source: searchResults,
+        templates: {
+          suggestion: function(value) {
+            return `
+              <div class="search-result diory-element__element">
+                  <div class="diory-element__title">
+                    ${value.value}
+                  </div>
+              </div>`
+          },
+          empty: function() {
+            return `<div class="search-result diory-element__element">
+                      <div class="diory-element__title">No results.</div>
+                    </div>`
+          }
+        }
+    });
+
+    // X click
+    $('#search-create__cancel').click(() => {
+      $('#typeahead').typeahead('val', '');
+    })
+
+    // Loading starts
+    $('#search-create').bind('typeahead:asyncrequest', function() {
+      $('#loading-icon').css('display', 'block');
+    });
+
+    // Loading ends
+    $('#search-create').bind('typeahead:asyncreceive', function() {
+      $('#loading-icon').hide();
+    });
+
+    // Loadinc cancelled (esc)
+    $('#search-create').bind('typeahead:asynccancel', function() {
+      $('#loading-icon').hide();
+    });
+
+}
+

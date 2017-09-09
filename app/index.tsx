ce4ffe8f8309acc7109413d20b070a2834adeb57
Jouni Alanen <jouni.alanen@leadin.fi>
2017-09-09 15:48:08 +0300

    Grid added, index.tsx rewritten and preparing for search-create component

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,87 @@ import * as ReactDOM from 'react-dom';
 import { Diory, DioryGrid } from 'diory-react-components'
 import { DiographStore } from "diograph-store"
 import { DiographAuthentication } from "diograph-authentication"
+import { SearchCreate } from "./search-create"
 
-import { DiographSearchCreate } from "diograph-search-create"
-let becauseItMustBeUsedToBeIncludedIntoBundle = DiographSearchCreate;
+class App extends React.Component {
+  state
 
-render()
+  constructor(props) {
+    super(props)
+
+    DiographStore.setAuthToken(DiographAuthentication.token);
+
+    this.state = {diory: {text: "COOOL"}, diories: {}}
+
+    this.loadDiories()
+
+    this.putInFocus = this.putInFocus.bind(this);
+  }
+
+  loadDiories() {
+    DiographStore.getAllDiories().then((diories) => {
+      if (diories.length < 1) { return; }
+
+      const gridDiory = this.convertDioryToGridDiory(diories[0])
+
+      const gridDiories = {
+        text: 'This is a grid:',
+        image: 'https://gravatar.com/avatar/ff80f8f9bc52f1b79e468a41f2239001',
+        styles: {
+          text: { fontSize: '2em', fontFamily: 'sans-serif', color: 'white' }
+        },
+        diorys: this.convertDioriesToGridDiories(diories)
+      }
+
+      this.setState({diory: gridDiory})
+      this.setState({diories: gridDiories})
+    })
+  }
+
+  // <SearchCreate onFocusClick={this.putInFocus}/>
+
+  render() {
+    let diory = this.state.diory
+    let diories = this.state.diories
+    return (
+      <div>
+        <SearchCreate />
+        <h1>{diory.text}</h1>
+        <Diory { ...diory } />
+        <DioryGrid { ...diories } />
+      </div>
+    )
+  }
+
+  putInFocus(dioryId) {
+    console.log(dioryId)
+    DiographStore.getDiory(dioryId).then(diory => {
+      this.setState({diory: this.convertDioryToGridDiory(diory)})
+    })
+
+  }
+
+  convertDioryToGridDiory(diory) {
+    return {
+      text: diory.name,
+      image: 'https://gravatar.com/avatar/ff80f8f9bc52f1b79e468a41f2239001',
+      styles: {
+        diory: { display: 'inline-block', width: '20em', height: '20em', backgroundColor: 'green', margin: '1em' },
+        text: { fontSize: '2em', fontFamily: 'sans-serif', color: 'white', textAlign: 'center', textShadow: '1px 1px green' },
+        image: { opacity: 0.6, filter: 'blur(5px)' }
+      }
+    }
+  }
+
+  convertDioriesToGridDiories(diories) {
+    let gridDiories = {}
+    diories.forEach(diory => {
+      gridDiories[parseInt(diory.id)] = this.convertDioryToGridDiory(diory)
+    })
+    return gridDiories
+  }
+
+}
 
 DiographAuthentication.onLogin = () => {
   render()
@@ -17,39 +93,6 @@ DiographAuthentication.onLogout = () => {
   clear()
 }
 
-function render() {
-  DiographStore.setAuthToken(DiographAuthentication.token);
-
-  try {
-    DiographStore.getAllDiories().then((dioryData) => {
-      if (dioryData.length < 1) { return; }
-
-      dioryData = dioryData[0]
-
-      const diory = {
-        text: dioryData.name,
-        image: 'https://gravatar.com/avatar/ff80f8f9bc52f1b79e468a41f2239001',
-        styles: {
-          diory: { display: 'inline-block', width: '20em', height: '20em', backgroundColor: 'green', margin: '1em' },
-          text: { fontSize: '2em', fontFamily: 'sans-serif', color: 'white', textAlign: 'center', textShadow: '1px 1px green' },
-          image: { opacity: 0.6, filter: 'blur(5px)' }
-        }
-      }
-
-      ReactDOM.render(
-        <div>
-          <h1>{ dioryData.name }</h1>
-          <Diory { ...diory } />
-        </div>,
-        document.getElementById('app')
-      );
-
-    })
-  } catch(e) {
-    console.log(e)
-  }
-}
-
 function clear() {
   ReactDOM.render(
     <div><p>No diories to show.</p></div>,
@@ -57,3 +100,11 @@ function clear() {
   );
 }
 
+function render() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+}
+
+render()

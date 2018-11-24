08814f4ed18a10ddc936743fb5526d47ad10f49d
Jouni Alanen <jvalanen@gmail.com>
2018-11-24 22:41:15 +0200

    Webpack versions updated + config file + dev-server settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: "./app/index.tsx",
-    output: {
-        filename: "./dist/bundle.js",
-        path: __dirname,
-    },
-    module: {
-        loaders: [
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    }
+  mode: "development",
+  entry: "./app/index.tsx",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [ { loader: "ts-loader" } ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      'DIOGRAPH_SERVER_HOST': 'http://localhost:3000'
+    })
+  ],
+  // Example taken from https://medium.com/code-oil/burning-questions-with-answers-to-why-webpack-dev-server-live-reload-does-not-work-6d6390277920
+  devServer: {
+    publicPath: '/',
+    contentBase: "dist",
+    watchContentBase: true,
+    compress: true,
+    port: 4202
+  }
 };
